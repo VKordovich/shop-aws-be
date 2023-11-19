7b0bad6b400c13ab184905e7fa675f24da3cd601
vkordovich <vadim_kordovich@epam.com>
2023-11-20 00:20:44 +0300

    task-10

---
+++ bff-service/src/app.controller.ts
@@ -0,0 +1,18 @@
+import {
+  Controller,
+  Get,
+  Req
+} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(
+      private readonly appService: AppService
+  ) {}
+
+  @Get(['*'])
+  getHello(@Req() request: Request): unknown {
+    return this.appService.getHello(request);
+  }
+}
